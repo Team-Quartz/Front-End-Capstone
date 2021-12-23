3e06cc950186947138bdf99c430c31f73f40c8db
Liam Richardson <liam@richardsonconcepts.com>
2021-12-23 16:26:21 -0500

    Added utils import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import ProductDetails from './components/product-details/Index.jsx';
 import QuestionsAndAnwsers from './components/questions-and-answers/Index.jsx';
 import RatingsAndReviews from './components/ratings-and-reviews/Index.jsx';
 import RelatedItemsAndComparisons from './components/shared-components/Index.jsx';
+import utils from './utils/Utils.js';
 
 class App extends react.Component {
   constructor(props) {
