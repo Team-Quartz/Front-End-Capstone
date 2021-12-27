a7f06cdb691a503327c329eea1849a27ea652784
Liam Richardson <liam@richardsonconcepts.com>
2021-12-27 13:26:38 -0500

    Added functions to load placeholder data and, eventually, actual data

--- client/src/components/ratings-and-reviews/Index.jsx
+++ client/src/components/ratings-and-reviews/Index.jsx
@@ -1,12 +1,39 @@
 import react from 'react';
+import placeholder from './placeholderData.js';
+
+const blankState = {
+  loadedReviews: [],
+  filters: [],
+  reviewPage: 0,
+};
 
 class RatingsAndReviews extends react.Component {
   constructor(props) {
     super(props);
-    this.state={
-      loadedReviews: [],
-      filters: []
-    }
+  }
+
+  componentDidMount() {
+    this.loadNewProduct();
+    this.loadReviews();
+  }
+
+  loadNewProduct() {
+    //TODO: get current product from props
+    this.setState(blankState);
+  }
+
+  loadReviews() {
+    //TODO: get current product from props
+    this.setState((state, props) => {
+      const reviewPage = ++state.reviewPage;
+      const loadedReviews = state.loadedReviews.concat(placeholder.reviews.results);
+      return { loadedReviews, reviewPage };
+    })
+  }
+
+  loadMeta() {
+    //TODO: get current product from props
+    return placeholder.reviewsMeta;
   }
 
   render() {
@@ -15,7 +42,8 @@ class RatingsAndReviews extends react.Component {
         <h2>Ratings &amp; Reviews</h2>
         <div>
           <div className='starsAndBars'>
-            <div>3.5 * * * _ _</div>{/** replace with stars component */}
+            <div>3.5 * * * _ _</div>
+            {/** replace with stars component */}
             <div>100% of reviews recommend this product</div>
             <div className='starsFilters'>
               <div>5 stars ---------____</div>
@@ -24,15 +52,11 @@ class RatingsAndReviews extends react.Component {
               <div>2 stars ---------____</div>
               <div>1 stars ----_________</div>
             </div>
-            <div>
-
-            </div>
+            <div></div>
           </div>
           <div className='reviewsList'>
             <div>248 reviews, sorted by relevance</div>
-            <div>
-              {/* reviews list */}
-            </div>
+            <div>{/* reviews list */}</div>
             <div>
               <button>MORE REVIEWS</button>
               <button>ADD A REVIEW +</button>
@@ -40,7 +64,7 @@ class RatingsAndReviews extends react.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
