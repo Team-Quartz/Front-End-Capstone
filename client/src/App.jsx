58fe48593b6726c9acfe569b546494195c08d89a
Liam Richardson <liam@richardsonconcepts.com>
2021-12-27 12:36:58 -0500

    Prettified App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
-import react from 'react';
-import reactDOM from 'react-dom';
+import react from "react";
+import reactDOM from "react-dom";
 
-import ProductDetails from './components/product-details/Index.jsx';
-import QuestionsAndAnwsers from './components/questions-and-answers/Index.jsx';
-import RatingsAndReviews from './components/ratings-and-reviews/Index.jsx';
-import RelatedItemsAndComparisons from './components/related-items-and-comparisons/Index.jsx';
-import utils from './Utils.js';
+import ProductDetails from "./components/product-details/Index.jsx";
+import QuestionsAndAnwsers from "./components/questions-and-answers/Index.jsx";
+import RatingsAndReviews from "./components/ratings-and-reviews/Index.jsx";
+import RelatedItemsAndComparisons from "./components/related-items-and-comparisons/Index.jsx";
+import utils from "./Utils.js";
 
 class App extends react.Component {
   constructor(props) {
     super(props);
-    this.state = {/** to be determined */}
-  };
+    this.state = {
+      /** to be determined */
+    };
+  }
 
   render() {
     return (
@@ -23,7 +25,7 @@ class App extends react.Component {
         {/* <RelatedItemsAndComparisons /> */}
       </div>
     );
-  };
+  }
 }
 
-reactDOM.render(<App />, document.getElementById('app'));
+reactDOM.render(<App />, document.getElementById("app"));
