7d9dabf5ccf44f2ba524baf450ce96f721dd25b8
Liam Richardson <liam@richardsonconcepts.com>
2021-12-26 21:03:46 -0500

    Added missing dependencies, fixed react being in dev-dependencies, fixed addon that was creating junk files in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 
 module.exports = {
   entry: "./client/src/App.jsx",
-  mode: "production",
+  mode: "development",
   devtool: "source-map",
   module: {
     rules: [
@@ -22,14 +22,6 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "client/dist"),
-    publicPath: "client/dist",
     filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  }
 };
