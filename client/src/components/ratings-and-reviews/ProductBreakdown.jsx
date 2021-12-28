17d6d7fe6bbb7ad2fa30e5f04bf8bc2bc6e63f0d
Liam Richardson <liam@richardsonconcepts.com>
2021-12-28 16:41:03 -0500

    Added bars and triangles, used CSS to place them based on characteristic score

--- client/src/components/ratings-and-reviews/ProductBreakdown.jsx
+++ client/src/components/ratings-and-reviews/ProductBreakdown.jsx
@@ -1,13 +1,31 @@
 import react from 'react';
+import styled from 'styled-components';
+
+const Characteristic = styled.div`
+  position: relative;
+`
+const BarFrame = styled.div`
+  & {
+    background-color: LightGrey;
+    height: 8px;
+  }
+  &:before {
+    content: "▼";
+    position: absolute;
+    left: ${props => props.position * 100}%;
+    transform: translateX(-50%);
+  }
+`;
 
 function ProductBreakdown({ characteristics }) {
   return (
     <div>
       {Object.entries(characteristics).map(([characteristic, { id, value }]) => {
         return (
-          <div key={id}>
-            {characteristic}, {value}
-          </div>
+          <Characteristic key={id}>
+            {characteristic}
+            <BarFrame position={(value - 1) / 4}/>
+            </Characteristic>
         );
       })}
     </div>
