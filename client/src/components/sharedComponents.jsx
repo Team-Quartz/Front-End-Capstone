24693e5cc9a50c44a40eb9552cd9fa47143b996b
Liam Richardson <liam@richardsonconcepts.com>
2021-12-29 14:22:51 -0500

    Implemented stars image

--- client/src/components/sharedComponents.jsx
+++ client/src/components/sharedComponents.jsx
@@ -1,6 +1,35 @@
 import react from 'react';
+import styled from 'styled-components';
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StarBounds = styled.div`
+  width: 1em;
+  height: 1em;
+  overflow: hidden;
+  position: relative;
+`;
+
+const StarImg = styled.img`
+  position: absolute;
+  top: 0;
+  left: -${({ amount }) => amount}em;
+  height: 1em;
+  width: 5em;
+`;
 
 export const Stars = ({ reviewsMeta }) => {
-  const ratingClipped = Math.floor(reviewsMeta.averageRating * 4) / 4;
-  return <div>{ratingClipped}</div>;
+  const ratingClipped = Math.floor(reviewsMeta.averageRating * 4);
+  const stars = [];
+  for (let i = 0; i < 20; i += 4) {
+    stars.push(
+      <StarBounds key={i}>
+        <StarImg src='./img/stars.png' amount={Math.max(0, Math.min(4, ratingClipped - i))}/>
+      </StarBounds>
+    );
+  }
+  return <FlexRow>{stars}</FlexRow>;
 };
