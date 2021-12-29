743eba050c9f41fb9256fea42f5d6794137da41f
Liam Richardson <liam@richardsonconcepts.com>
2021-12-29 15:30:46 -0500

    Add stars to reviews

--- client/src/components/ratings-and-reviews/ReviewsList.jsx
+++ client/src/components/ratings-and-reviews/ReviewsList.jsx
@@ -1,9 +1,13 @@
 import react from 'react';
+import { Stars } from '../sharedComponents.jsx';
 
 function ReviewsList({ reviews }) {
   return (<div>{
     reviews.map(review => {
-      return <div key={review.review_id}>{review.summary}</div>
+      return <div key={review.review_id}>
+        <Stars reviewsMeta={{ averageRating: review.rating }}/>
+        {review.summary}
+      </div>
     })
   }</div>);
 };
