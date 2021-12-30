662b547a50d2c24581ff3ce303bae09658d32a62
Javier Tamez <tamezjavier@gmail.com>
2021-12-29 21:47:55 -0600

    Added some styled components for the ProductInformation.jsx.

--- client/src/components/product-details/ProductInformation.jsx
+++ client/src/components/product-details/ProductInformation.jsx
@@ -1,18 +1,32 @@
-const ProductInformation = ({productData}) => {
-  const {category, name, default_price, description} = productData;
+import styled from 'styled-components';
+
+
+const ProductInfoDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Descriptors = styled.div`
+  font-family: "Arial", "Helvetica Neue", sans-serif;
+  font-size: 12px;
+`
+const CategoryStyle = styled(Descriptors)`
+  font-size: 24px;
+`;
+const ProductInformation = ({ productData }) => {
+  const { category, name, default_price, description } = productData;
   /*
   TODO: create conditional rendering for price and style accordingly. Create links for social media
   Note: currently do not have access to number of stars and need to look into that. Also needs a way to make links for social media
   */
   return (
-    <div className="product-info">
+    <ProductInfoDiv>
       <div className="review-stars"></div>
-      <div className="category-name">{category}</div>
-      <div className="product-name">{name}</div>
-      <div className="product-price">${default_price}</div>
-      <div className="product-full-description">{description}</div>
-      <div className="social-media-links">Social Media placeholder</div>
-    </div>
+      <CategoryStyle>{category}</CategoryStyle>
+      <Descriptors>${default_price}</Descriptors>
+      <Descriptors>{description}</Descriptors>
+      <Descriptors>Social Media placeholder</Descriptors>
+    </ProductInfoDiv>
   )
 }
 export default ProductInformation;
