fd3d316a46203270681dffc56f0d20af2d3fa663
Liam Richardson <liam@richardsonconcepts.com>
2021-12-30 11:18:37 -0500

    Change import order for more consistent style overriding behavior

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import react from 'react';
 import reactDOM from 'react-dom';
 
+//  note: it's important that sharedComponents be imported early in App,
+//  so its styles get added before any other modules (for consistent overriding behavior)
+import { AppStyle } from './components/sharedComponents.jsx';
+
 import ProductDetails from './components/product-details/Index.jsx';
 import QuestionsAndAnwsers from './components/questions-and-answers/Index.jsx';
 import RatingsAndReviews from './components/ratings-and-reviews/Index.jsx';
 import RelatedItemsAndComparisons from './components/related-items-and-comparisons/Index.jsx';
 import utils from './Utils.js';
 import { reviewsMeta } from './placeholderData.js';
-import { AppStyle } from './components/sharedComponents.jsx';
 
 class App extends react.Component {
   constructor(props) {
