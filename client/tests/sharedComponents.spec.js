e70ec3456b7651d29eb021cf9ff10f71c75b7070
Liam Richardson <liam@richardsonconcepts.com>
2021-12-31 10:31:59 -0500

    Moved tests to tests folder

--- client/tests/sharedComponents.spec.js
+++ client/tests/sharedComponents.spec.js
@@ -3,21 +3,19 @@
  */
 import React from 'react';
 import reactDOM from 'react-dom';
-import { Stars } from './sharedComponents.jsx';
+import { Stars } from '../src/components/sharedComponents.jsx';
 
 describe('Shared Components', () => {
   let stars;
   beforeAll(() => {
     stars = document.createElement('div');
     document.body.appendChild(stars);
-    reactDOM.render(<Stars reviewsMeta={{ averageRating: 4.25 }} />, stars);
+    reactDOM.render(<Stars reviewsMeta={{ averageRating: 3.25 }} />, stars);
   });
   describe('Stars component', () => {
+    //TODO: replace this test with a DOM snapshot
     it('should have five children, nested in a div', () => {
       expect(stars.children[0].children.length).toEqual(5);
-    });
-    it('should have images as children', () => {
-      expect(stars.children[0].children[0].children[0].tagName).toEqual('IMG');
     });
   });
 });
