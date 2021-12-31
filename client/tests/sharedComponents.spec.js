f4f74dea7cbf156eeac99a308d47ad0453eb7ed5
Liam Richardson <liam@richardsonconcepts.com>
2021-12-31 10:55:27 -0500

    Added jsdom tests, regenerated snapshot

--- client/tests/sharedComponents.spec.js
+++ client/tests/sharedComponents.spec.js
@@ -15,7 +15,15 @@ describe('Shared Components', () => {
     document.body.appendChild(stars);
     reactDOM.render(<Stars reviewsMeta={{ averageRating: 3.25 }} />, stars);
   });
-  describe('Stars component', () => {
+  describe('Stars component, jsdom', () => {
+    it('should have five children, nested in a div', () => {
+      expect(stars.children[0].children.length).toEqual(5);
+    });
+    it('should have images as children', () => {
+      expect(stars.children[0].children[0].children[0].tagName).toEqual('IMG');
+    });
+  });
+  describe('Stars component, react-test-renderer', () => {
     it('renders correctly', () => {
       //this test uses react-test-renderer to generate a json object version of the DOM
       //the snapshot method compares it to a saved, known-correct version
