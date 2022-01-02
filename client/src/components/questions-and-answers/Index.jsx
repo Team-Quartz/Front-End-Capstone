b86f8e9393b42c7a75edf56c3389a6065bf5ded9
Himmat Khalsa <himmatsinghkhalsa@gmail.com>
2022-01-02 12:51:35 -0700

    fill skeleton for main component

---
+++ client/src/components/questions-and-answers/Index.jsx
@@ -0,0 +1,32 @@
+import react from 'react';
+
+class QuestionsAndAnswers extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productId: null,
+      searchFilter: ''
+    }
+    //function bindings
+    this.updateSearchFilter = this.updateSearchFilter.bind(this);
+  }
+
+  componentDidMount() {
+    //load current product
+  }
+
+  updateSearchFilter(query) {
+    this.setState({ productId: filter })
+    //need to pass new filter to QuestionsList - might be done after automatic re-render
+  }
+
+  render() {
+    return(
+      <div>
+        {/* <SearchBar updateSearchFilter={this.updateSearchFilter} /> */}
+        {/* <QuestionsList productId={this.state.productId} searchFilter={this.state.searchFilter}/> */}
+      </div>
+    )
+  }
+}
+
