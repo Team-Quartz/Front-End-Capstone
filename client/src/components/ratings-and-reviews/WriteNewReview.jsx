8cfdeaf0bbd09e015da66be5292574e28ab80a40
Liam Richardson <liam@richardsonconcepts.com>
2022-01-02 14:46:30 -0500

    Add form, recommend yes/no radio buttons

--- client/src/components/ratings-and-reviews/WriteNewReview.jsx
+++ client/src/components/ratings-and-reviews/WriteNewReview.jsx
@@ -16,34 +16,39 @@ export default function WriteNewReview({ onClose, show}) {
   //DisplayError
   return (
     <Modal onClose={onClose} show={show}>
-      <h2>Write Your Review</h2>
-      <h3>About the {`[product name]`}</h3>
-      <div>{`[star rating]`}</div>
-      <div className='FlexRow'>
-        Do you recommend this product? Yes No
-      </div>
-      <div>
-        {`[characteristics]`}
-      </div>
-      <div>
-        Review Summary
-      </div>
-      <div>
-        Review Body
-      </div>
-      <div>
-        Upload your Photos
-      </div>
-      <div>
-        Nickname
-      </div>
-      <div>
-        Email
-      </div>
-      <div>
-        {`[error printout]`}
-        Submit
-      </div>
+      <form>
+        <h2>Write Your Review</h2>
+        <h3>About the {`[product name]`}</h3>
+        <div>{`[star rating]`}</div>
+        <div className='FlexRow'>
+          Do you recommend this product? Yes
+          <input type='radio' id='yes' name='recommend' />
+          No
+          <input type='radio' id='no' name='recommend' />
+        </div>
+        <div>
+          {`[characteristics]`}
+        </div>
+        <div>
+          Review Summary
+        </div>
+        <div>
+          Review Body
+        </div>
+        <div>
+          Upload your Photos
+        </div>
+        <div>
+          Nickname
+        </div>
+        <div>
+          Email
+        </div>
+        <div>
+          {`[error printout]`}
+          Submit
+        </div>
+      </form>
     </Modal>
   )
 }
