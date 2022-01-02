23d90ef4c57456c60ce9ec190b0b1329771213ed
Liam Richardson <liam@richardsonconcepts.com>
2022-01-02 13:18:18 -0500

    Updated structure

--- client/src/components/ratings-and-reviews/WriteNewReview.jsx
+++ client/src/components/ratings-and-reviews/WriteNewReview.jsx
@@ -12,33 +12,35 @@ export default function WriteNewReview(props) {
   //Email
   //DisplayError
   return (
-    <h2>Write Your Review</h2>
-    <h3>About the {`[product name]`}</h3>
-    <div>{`[star rating]`}</div>
-    <div className='FlexRow'>
-      Do you recommend this product? Yes No
-    </div>
     <div>
-      {`[characteristics]`}
-    </div>
-    <div>
-      Review Summary
-    </div>
-    <div>
-      Review Body
-    </div>
-    <div>
-      Upload your Photos
-    </div>
-    <div>
-      Nickname
-    </div>
-    <div>
-      Email
-    </div>
-    <div>
-      {`[error printout]`}
-      Submit
+      <h2>Write Your Review</h2>
+      <h3>About the {`[product name]`}</h3>
+      <div>{`[star rating]`}</div>
+      <div className='FlexRow'>
+        Do you recommend this product? Yes No
+      </div>
+      <div>
+        {`[characteristics]`}
+      </div>
+      <div>
+        Review Summary
+      </div>
+      <div>
+        Review Body
+      </div>
+      <div>
+        Upload your Photos
+      </div>
+      <div>
+        Nickname
+      </div>
+      <div>
+        Email
+      </div>
+      <div>
+        {`[error printout]`}
+        Submit
+      </div>
     </div>
   )
 }
