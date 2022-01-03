45c7e8d9509c62c785ab85c3a1c2ffe01d5b152a
Javier Tamez <tamezjavier@gmail.com>
2022-01-03 13:55:32 -0600

    Progress merging. WIP

---
+++ client/src/components/product-details/StyleSelectorMaker.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const StyleSelectorMaker = (styleObject, handler) => {
+  const StyledImage = styled.img`
+  width: 75px;
+  height: 75px;
+  `
+  return (
+    <StyledImage src={styleObject.photos[0].thumbnail_url} key={styleObject.style_id + ''} id={styleObject.style_id + '#'} onClick={(e) => {handler(e.target.getAttribute('id'))}} />
+  )
+}
+
+
+/*
+adding images needs to make images and wrap them. Need a wrapper and an array
+
+make an array of images from data
+Then wrap each into an event handler.
+*/
+export default StyleSelectorMaker;
