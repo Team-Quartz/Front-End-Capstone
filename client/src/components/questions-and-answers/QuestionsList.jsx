dae23ab157308052a39d61e58b4d11bc6cfdc27c
Himmat Khalsa <himmatsinghkhalsa@gmail.com>
2022-01-03 13:47:30 -0700

    remove conditional rendering in favor of show attribute. remove old comments

--- client/src/components/questions-and-answers/QuestionsList.jsx
+++ client/src/components/questions-and-answers/QuestionsList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { dummyData } from './dummyData.js';
 import QuestionEntry from './QuestionEntry.jsx';
 import QuestionModal from './QuestionModal.jsx';
-// import { Modal } from '../sharedComponents.jsx';
 
 class QuestionsList extends React.Component {
   constructor(props) {
@@ -12,12 +11,10 @@ class QuestionsList extends React.Component {
       questions: dummyData.results,
       questionCount: 2,
       searchFilter: this.props.searchFilter,
-      isQuestionModal: false,
       writeNewQuestion: false
     }
     //function bindings
     this.showMoreQuestions = this.showMoreQuestions.bind(this);
-    // this.showQuestionModal = this.showQuestionModal.bind(this);
     this.openQuestionModal = this.openQuestionModal.bind(this);
   }
 
@@ -31,11 +28,6 @@ class QuestionsList extends React.Component {
     this.setState((prevState, props) => ({ questionCount: prevState.questionCount + 2 }));
   }
 
-  // showQuestionModal() {
-  //   console.log('should pop up modal', this.state.isQuestionModal);
-  //   this.setState((prevState, props) => ({ isQuestionModal: !prevState.isQuestionModal }));
-  // }
-
   openQuestionModal(open) {
     this.setState({
       writeNewQuestion: open,
@@ -47,20 +39,15 @@ class QuestionsList extends React.Component {
       <div>
         {this.state.questions.slice(0, this.state.questionCount).map((question, idx) => {
           return <QuestionEntry key={idx} question={question} />
-          // return <p key={idx}>Q: {question.question_body}</p>
         })}
         {this.state.questions.length > this.state.questionCount
         ? <button onClick={this.showMoreQuestions}>MORE ANSWERED QUESTIONS</button>
         : null}
-        {this.state.writeNewQuestion
-          //TODO: create AddQuestionModal (use Liam's from shared components)
-        ? <QuestionModal
-            onClose={() => this.openQuestionModal(false)}
-            show={this.state.writeNewQuestion}
-            productName={this.state.productName}
-          />
-        : null
-        }
+        <QuestionModal
+          onClose={() => this.openQuestionModal(false)}
+          show={this.state.writeNewQuestion}
+          productName={this.state.productName}
+        />
         <button onClick={() => this.openQuestionModal(true)}>ADD A QUESTION +</button>
       </div>
     )
