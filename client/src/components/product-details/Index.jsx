45c92206e83a046feb3b8d195128da6b2a93f093
Javier Tamez <tamezjavier@gmail.com>
2022-01-04 11:10:43 -0600

    Adjusting code to account for lack of preloaded data.

--- client/src/components/product-details/Index.jsx
+++ client/src/components/product-details/Index.jsx
@@ -9,22 +9,21 @@ class ProductDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      stylesData,
-      productData,
-      selectedStyle: null,
-      highlightStyle: null,
+      stylesData: [],
+      productData: {},
+      selectedStyle: {},
+      highlightStyle: '0',
     }
     this.StyleSelectorHandler = this.StyleSelectorHandler.bind(this);
   }
   StyleSelectorHandler (targetKey) {
-    targetKey = targetKey.substring(0, targetKey.length - 1);
-    const matchedStyle = this.state.stylesData.results.find((styleObject) => {
+    const matchedStyle = this.state.stylesData.find((styleObject) => {
       return styleObject.style_id + '' === targetKey
     })
     this.setState({selectedStyle: matchedStyle, highlightStyle: targetKey});
   }
   componentDidMount () {
-    this.setState({selectedStyle: stylesData.results[0]});
+    this.setState({selectedStyle: stylesData.results[0], stylesData: stylesData.results, productData: productData});
   }
   render() {
     const starProp = <Stars reviewsMeta={this.props.reviewsMeta}/>;
@@ -33,15 +32,12 @@ class ProductDetail extends React.Component {
     <div>
         <ImageGallery data={this.state.selectedStyle}/>
         <ProductInformation productData={this.state.productData} starsData={starProp} selectedStyle={this.state.selectedStyle}/>
-        <StyleSelector stylesData={this.state.stylesData.results} handler={this.StyleSelectorHandler}/>
+        <StyleSelector stylesData={this.state.stylesData} handler={this.StyleSelectorHandler}/>
         <AddToCart />
     </div>
     );
   }
 }
 
-//<ImageGallery data={{}/* current style selection data */}/>
-//<ProductInformation productData={this.productData} starsData={{}}/> {/* need to pass in data for markdown price and number of stars data*/}
-//<StyleSelector stylesData={this.stylesData}/> {/* need to pass in event handler */}
-//<AddToCart /> {/* need to pass in event handler and current styles data */}
+
 export default ProductDetail;
