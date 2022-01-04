2fd156a04b0ca53e6c9f8b2df01cbe420b99f6be
Javier Tamez <tamezjavier@gmail.com>
2022-01-04 14:13:40 -0600

    bad merge
    Revert "Adjusting code to account for lack of preloaded data."
    
    This reverts commit 45c92206e83a046feb3b8d195128da6b2a93f093.

--- client/src/components/product-details/ImageGallery.jsx
+++ client/src/components/product-details/ImageGallery.jsx
@@ -1,39 +1,21 @@
 import styled from 'styled-components';
-import React from 'react';
 const StyledImage = styled.img`
 height: 300;
 width: 300;
 `
 
+const ImageGallery = ({data}) => {
+  let source = "https://media.giphy.com/media/xitrfnahXHFZi5giQs/giphy.gif";
 
-class ImageGallery extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: props.data.photos,
+  if (data !== null) {
+    source = data.photos[0].url;
     }
-  }
 
-  render () {
   return (
     <div>
-      <StyledImage className="gallery" src={this.state.images[0].url} alt="A lot of random images"/>
+      {/* <img className="gallery" src="https://media.giphy.com/media/xitrfnahXHFZi5giQs/giphy.gif" alt="A lot of random images"/> */}
+      <StyledImage className="gallery" src={source} alt="A lot of random images"/>
     </div>
   )
-  }
 }
 export default ImageGallery;
-
-
-/*
-    if(!!option) {
-      const currentIndex = this.stylesData.results.findIndex(this.selectedStyle.style_id);
-      if (option === 'previous') {
-        const minZero = Math.max(currentIndex - 1, 0);
-        const targetStyle = this.stylesData.results[minZero];
-        this.setState({selectedStyle: targetStyle, targetStyle.style_id})
-      } else if (option === 'next') {
-        const maxLimit = Math.min(currentIndex + 1, this.stylesData.results)
-      }
-    }
-*/
--- client/src/components/product-details/Index.jsx
+++ client/src/components/product-details/Index.jsx
@@ -16,21 +16,29 @@ class ProductDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+<<<<<<< HEAD
       stylesData: [null],
       productData: {data: null},
       selectedStyle: {data: null},
       highlightStyle: '0',
+=======
+      stylesData,
+      productData,
+      selectedStyle: null,
+      highlightStyle: null,
+>>>>>>> parent of 45c9220 (Adjusting code to account for lack of preloaded data.)
     }
     this.StyleSelectorHandler = this.StyleSelectorHandler.bind(this);
   }
   StyleSelectorHandler (targetKey) {
-    const matchedStyle = this.state.stylesData.find((styleObject) => {
+    targetKey = targetKey.substring(0, targetKey.length - 1);
+    const matchedStyle = this.state.stylesData.results.find((styleObject) => {
       return styleObject.style_id + '' === targetKey
     })
     this.setState({selectedStyle: matchedStyle, highlightStyle: targetKey});
   }
   componentDidMount () {
-    this.setState({selectedStyle: stylesData.results[0], stylesData: stylesData.results, productData: productData});
+    this.setState({selectedStyle: stylesData.results[0]});
   }
   render() {
     const starProp = <Stars reviewsMeta={this.props.reviewsMeta}/>;
@@ -42,7 +50,11 @@ class ProductDetail extends React.Component {
         <div>
         <ImageGallery data={this.state.selectedStyle}/>
         <ProductInformation productData={this.state.productData} starsData={starProp} selectedStyle={this.state.selectedStyle}/>
+<<<<<<< HEAD
         <StyleSelector stylesData={this.state.stylesData} handler={this.StyleSelectorHandler} />
+=======
+        <StyleSelector stylesData={this.state.stylesData.results} handler={this.StyleSelectorHandler}/>
+>>>>>>> parent of 45c9220 (Adjusting code to account for lack of preloaded data.)
         <AddToCart />
         </div>}
     </div>
@@ -50,5 +62,8 @@ class ProductDetail extends React.Component {
   }
 }
 
-
+//<ImageGallery data={{}/* current style selection data */}/>
+//<ProductInformation productData={this.productData} starsData={{}}/> {/* need to pass in data for markdown price and number of stars data*/}
+//<StyleSelector stylesData={this.stylesData}/> {/* need to pass in event handler */}
+//<AddToCart /> {/* need to pass in event handler and current styles data */}
 export default ProductDetail;
