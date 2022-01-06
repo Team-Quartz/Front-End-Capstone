2dd709e307902b782d5358b11c12946a3df795e7
Liam Richardson <liam@richardsonconcepts.com>
2022-01-06 13:25:53 -0500

    Added sort

--- client/src/components/ServerMetrics.jsx
+++ client/src/components/ServerMetrics.jsx
@@ -6,10 +6,18 @@ export default function ServerMetrics() {
   const [total, setTotal] = React.useState(0);
   const [calls, setCalls] = React.useState({});
   const [show, setShow] = React.useState(false);
+  const [sortBy, setSortBy] = React.useState('count');
+
+  function sort(a, b) {
+    if (sortBy === 'count') {
+      return b[1] - a[1];
+    }
+    return a[0] < b[0] ? -1 : 1;
+  }
 
   React.useEffect(() => {
     console.error(new Error('MAKE SURE TO REMOVE THIS COMPONENT BEFORE SHIPPING!'));
-  }, [])
+  }, []);
 
   React.useEffect(() => {
     if (show) {
@@ -33,13 +41,20 @@ export default function ServerMetrics() {
       <button onClick={() => setShow(true)}>Show Server Metrics</button>
       <Modal show={show} onClose={() => setShow(false)}>
         <button onClick={resetCount}>Reset calls count</button>
+        <label for='sortBy'>Sort by</label>
+        <select id='sortBy' name='sortBy' value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='count'>count</option>
+          <option value='endpoint'>endpoint</option>
+        </select>
         Total: {total}
         <div>
-          {Object.entries(calls).map(([endpoint, count]) => (
-            <div key={endpoint}>
-              {endpoint}: {count}
-            </div>
-          ))}
+          {Object.entries(calls)
+            .sort(sort)
+            .map(([endpoint, count]) => (
+              <div key={endpoint}>
+                {endpoint}: {count}
+              </div>
+            ))}
         </div>
       </Modal>
     </div>
