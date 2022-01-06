b51271a0338f16dddbff6cb01e21a5124b56dd5f
Liam Richardson <liam@richardsonconcepts.com>
2022-01-06 17:06:04 -0500

    Rudimentary cache

--- client/src/haxios.js
+++ client/src/haxios.js
@@ -1,7 +1,33 @@
-const axios = require('axios')
+const axios = require('axios');
 
-function get(...args) {
-  return axios.get(...args)
+const cache = {};
+
+function get(url, config) {
+  if (url.endsWith('/')) url = url.slice(0, url.length - 1);
+  if (!url.startsWith('/')) url = `/${url}`;
+
+  let paramString = '';
+  if (config && config.params) {
+    console.log(config.params);
+    paramString = `?${Object.entries(config.params)
+      .map(([key, val]) => `${key}=${val}`)
+      .join('&')}`;
+  }
+
+  const urlString = url + paramString;
+
+  if (cache[urlString]) {
+    return new Promise((resolve, reject) => {
+      console.log('using cache for ', urlString)
+      //hacky stringify-parse to deep copy the object
+      resolve(JSON.parse(JSON.stringify(cache[urlString])));
+    });
+  }
+  return axios.get(url, config).then((response) => {
+    const cacheData = { data: response.data };
+    cache[urlString] = cacheData;
+    return cacheData;
+  });
 }
 
 function post(...args) {
@@ -12,4 +38,4 @@ function put(...args) {
   return axios.put(...args);
 }
 
-module.exports={get, post, put};
+module.exports = { get, post, put };
